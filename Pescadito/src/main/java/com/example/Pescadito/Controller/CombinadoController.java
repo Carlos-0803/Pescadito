a39f74d7af79ea4793e0d6f8a22b717a6478a655
DalethCorreaAngeles <U23225481@utp.edu.pe>
2025-11-18 11:03:57 -0500

    modificado

--- Pescadito/src/main/java/com/example/Pescadito/Controller/CombinadoController.java
+++ Pescadito/src/main/java/com/example/Pescadito/Controller/CombinadoController.java
@@ -11,36 +11,35 @@ import java.util.List;
 public class CombinadoController {
     private final CombinadoService combinadoService;
 
-    public CombinadoController(CombinadoService combinadoService){
+    public CombinadoController(CombinadoService combinadoService) {
         this.combinadoService = combinadoService;
     }
 
     @GetMapping
-    public List<Combinado> obtenerCombinado(){
+    public List<Combinado> obtenerCombinado() {
         return combinadoService.listarTodas();
-       }
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Combinado>
-        obtenerCombinadoPorId(@PathVariable Long id){
-            return combinadoService.buscarPorID(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-        }
+    obtenerCombinadoPorId(@PathVariable Long id) {
+        return combinadoService.buscarPorID(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
-    public ResponseEntity<?> crearCombinado(@RequestBody Combinado combinado){
-        if(combinado.getNombre()== null || combinado.getNombre().trim().isEmpty()){
+    public ResponseEntity<?> crearCombinado(@RequestBody Combinado combinado) {
+        if (combinado.getNombre() == null || combinado.getNombre().trim().isEmpty()) {
             return ResponseEntity.badRequest().body("el campo nombre es obligatorio");
         }
-       if(combinado.getPrecio() == 0){
+        if (combinado.getPrecio() == 0) {
             return ResponseEntity.badRequest().body("el campo precio no debe ser 0");
         }
 
-        if(combinado.getDescripcion()== null){
+        if (combinado.getDescripcion() == null) {
             return ResponseEntity.badRequest().body("el campo descripcion es obligatorio");
         }
 
         return ResponseEntity.ok(combinadoService.crearCombinado(combinado));
 
     }
-
-
 }
